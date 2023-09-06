c5657f2ab498b24df3cbb52204d79ccdecdf0d61
YuliaPortnova <yuyul@yandex.ru>
2023-09-07 00:43:48 +0300

    feat: add header, hero-index and catalog  stylesheets

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,6 @@
-/* в этот файл добавляет скрипты*/
+const mainHeaderToggle = document.querySelector('.main-header__toggle');
+const mainНeader = document.querySelector('.main-header');
+
+mainНeader.classList.remove('no-js');
+
+mainHeaderToggle.addEventListener('click', () => mainНeader.classList.toggle('is-open'));
