25bb5bda0e953961539921b2bf5e20c406f4bb85
YuliaPortnova <yuyul@yandex.ru>
2023-09-09 18:55:51 +0300

    feat: add logo

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ export function createWebp () {
 }
 
 export function optimizeVector () {
-  return gulp.src(['source/img/**/*.svg', '!source/img/icons/**/*.svg', '!source/img/adaptive-svg/*.svg'])
+  return gulp.src(['source/img/**/*.svg', '!source/img/icons/**/*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'));
 }
@@ -87,7 +87,6 @@ export function copyAssets () {
     'source/fonts/**/*.{woff2,woff}',
     'source/*.ico',
     'source/*.webmanifest',
-    'source/img/content/adaptive-svg/logo-yagoda.svg',
   ], {
     base: 'source'
   })
